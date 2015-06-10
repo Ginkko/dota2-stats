9154c58aa22cc550da7c999dd80a6a8d1b04c433
Alec Arme & Toby Alden <student@epicodus.com>
2015-06-10 12:05:40 -0700

    Add ember select box for primary stats to new hero inputs.

--- controllers/heroes.js
+++ controllers/heroes.js
@@ -1,6 +1,9 @@
 Dota2Stats.HeroesController = Ember.ArrayController.extend({
   addingHero: false,
+  primaryStats: ["Int", "Agi", "Str"],
+
   actions: {
+
     addHero: function() {
       this.set('addingHero', true);
     }
