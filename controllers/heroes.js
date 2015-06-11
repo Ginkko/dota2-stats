1e2bb44786316e3f08b6b0798a351cc434d164d6
Alec Arme & Toby Alden <student@epicodus.com>
2015-06-11 10:25:17 -0700

    Add rudimentary checkbox observation."
    q

--- controllers/heroes.js
+++ controllers/heroes.js
@@ -1,7 +1,21 @@
+
+
 Dota2Stats.HeroesController = Ember.ArrayController.extend({
   heroFormToggled: false,
   primaryStats: ["Int", "Agi", "Str"],
   roles: ["Hard Carry", "Semi-Carry", "Support", "Roamer", "Ganker", "Initiator", "Durable", "Disabler"],
+  checkedRoles: [],
+
+  hardCarry:undefined,
+  watchHardCarry: function(){
+    console.log("Hard Carry:" + this.get('hardCarry'));
+    if (this.get('hardCarry')) {
+        this.get('checkedRoles').push("Hard Carry");
+    } else {
+      this.get('checkedRoles').splice( this.get('checkedRoles').indexOf('Hard Carry'), 1 );
+    }
+    console.log(this.get('checkedRoles'));
+  }.observes('hardCarry'),
 
   actions: {
 
@@ -10,13 +24,6 @@ Dota2Stats.HeroesController = Ember.ArrayController.extend({
     },
 
     saveHero: function() {
-      var roles = this.get('roles');
-      var checkedRoles = [];
-      for(var i = 0; i < roles.length; i++) {
-        if(this.get(roles[i])) {
-          checkedRoles.push(roles[i]);
-        }
-      }
       var newHero = this.store.createRecord('hero', {
         name: this.get('name'),
         primaryStat: this.get('primaryStat'),
@@ -24,7 +31,6 @@ Dota2Stats.HeroesController = Ember.ArrayController.extend({
       });
       newHero.save();
       this.set('heroFormToggled', false);
-      // this.transitionToRoute('home');
     }
 
   }
---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// $(document).ready(function() {
+//
+//   event.preventDefault();
+//
+//   var getCheckedRoles = function() {
+//     var checkedRoles = [];
+//     $(".roles").each(function() {
+//       if(this.checked) {
+//         checkedRoles.push(this.name);
+//       }
+//     });
+//     return checkedRoles;
+//   }
+//
+// });
