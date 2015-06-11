d96c62a43a15ee2b8449ef0ffa5c1bc26b11c706
Alec Arme & Toby Alden <student@epicodus.com>
2015-06-11 14:52:53 -0700

    Bugfix multiple hero add to team bug.

--- controllers/hero-info.js
+++ controllers/hero-info.js
@@ -2,8 +2,8 @@ Dota2Stats.HeroInfoController = Ember.ObjectController.extend({
   needs: ['games'],
   actions: {
     addRadiant: function() {
-      this.get('controllers.games.radiantTeam').pushObject(this);
-      debugger;
+      var currentHero = this.store.find('hero', this.get('id'));
+      this.get('controllers.games.radiantTeam').pushObject(currentHero);
     }
   }
 });
